5c6708e7539c822612fb40da7b6c2996879a68ce
Luo-Hong-Jun <luo.hong.jun@students.jedlik.eu>
2024-05-21 17:48:10 +0200

    quick safe

--- connect.js
+++ connect.js
@@ -1,5 +1,34 @@
 let table = document.querySelector("#table");
+let Side = true;
+let round = true;
 
 for (let index = 0; index < 42; index++) {
     table.innerHTML += `<div class="block" id="${index}"></div>`;
+}
+
+let cells = document.querySelectorAll('.block');
+
+cells.forEach(x => {
+    x.addEventListener("click", Test);
+});
+
+function Test() {
+    console.log(`Block id ${this.id} wanna kys`);
+    console.log(this.style.backgroundColor);
+        if (this.style.backgroundColor == '#d3d3d3') {
+            if (Side == true) {
+                this.style.backgroundColor = 'blue';
+                Side = false;
+                round = false;
+            }
+            else{
+                this.style.backgroundColor = 'red';
+                Side = true;
+                round = false;
+            }
+        }
+        else{
+            console.log("Foglalt bitch");
+        }
+    round = true;
 }

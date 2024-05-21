9957ff4538d4480a4176d1e584c02dfb5ceb15f0
Luo-Hong-Jun <luo.hong.jun@students.jedlik.eu>
2024-05-21 21:39:13 +0200

    Turn syst

--- connect.js
+++ connect.js
@@ -1,6 +1,7 @@
 let table = document.querySelector("#table");
 let Side = true;
 let round = true;
+let store = document.querySelector("#Store");
 
 for (let index = 0; index < 42; index++) {
     table.innerHTML += `<div class="block" id="${index}"></div>`;
@@ -13,8 +14,11 @@ cells.forEach(x => {
 });
 
 function Test() {
-    console.log(`Block id ${this.id} wanna kys`);
-    console.log(this.style.backgroundColor);
+    let Id = Number(this.id);
+    console.log(this.id)
+    console.log(`Block id ${Id} wanna kys`);
+    var ThisCell = document.querySelector(`#${Id}`);
+    console.log(ThisCell.style.backgroundColor);
         if (this.style.backgroundColor == '#d3d3d3') {
             if (Side == true) {
                 this.style.backgroundColor = 'blue';
@@ -31,4 +35,11 @@ function Test() {
             console.log("Foglalt bitch");
         }
     round = true;
+}
+
+store.addEventListener('click', Morshu)
+
+function Morshu() {
+    /* fodder */
+    console.log("sorry Link, come back later when you are a little-... mmm.... Richer");
 }

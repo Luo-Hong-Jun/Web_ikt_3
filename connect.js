d7b21a92db9979391a18176d5068f93f9439e7ad
Luo-Hong-Jun <luo.hong.jun@students.jedlik.eu>
2024-05-22 21:52:13 +0200

    Shitpost

--- connect.js
+++ connect.js
@@ -2,24 +2,27 @@ let table = document.querySelector("#table");
 let Side = true;
 let round = true;
 let store = document.querySelector("#Store");
+let MorshuLine = false;
 
 for (let index = 0; index < 42; index++) {
     table.innerHTML += `<div class="block" id="${index}"></div>`;
 }
 
+console.log(document.getElementById("1"))
+
 let cells = document.querySelectorAll('.block');
 
 cells.forEach(x => {
+    x.style.backgroundColor = 'lightgray';
     x.addEventListener("click", Test);
 });
 
 function Test() {
-    let Id = Number(this.id);
+    let Id = this.id;
     console.log(this.id)
     console.log(`Block id ${Id} wanna kys`);
-    var ThisCell = document.querySelector(`#${Id}`);
-    console.log(ThisCell.style.backgroundColor);
-        if (this.style.backgroundColor == '#d3d3d3') {
+    console.log(this.style.backgroundColor);
+        if (this.style.backgroundColor == 'lightgray') {
             if (Side == true) {
                 this.style.backgroundColor = 'blue';
                 Side = false;
@@ -37,9 +40,14 @@ function Test() {
     round = true;
 }
 
-store.addEventListener('click', Morshu)
+store.addEventListener('click', Morshu);
 
 function Morshu() {
     /* fodder */
-    console.log("sorry Link, come back later when you are a little-... mmm.... Richer");
+    document.querySelector("#shop").style.visibility = 'visible';
+    let morshu = document.querySelector("#left");
+    if (MorshuLine == false) {
+        morshu.style.backgroundImage = 'url(/Web_ikt_3/Pics/Open_first.gif)';
+        document.querySelector('#audio').innerHTML = '<audio src="/Web_ikt_3/line1.mp3" controls="controls" style="display: none;" autoplay></audio>';
+    }
 }

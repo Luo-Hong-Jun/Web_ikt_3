8a0b55b3faf657d7bc9c014042bf776509af9889
Luo-Hong-Jun <luo.hong.jun@students.jedlik.eu>
2024-05-27 21:50:02 +0200

    upload

--- connect.js
+++ connect.js
@@ -1,28 +1,51 @@
 let table = document.querySelector("#table");
 let Side = true;
-let round = true;
 let store = document.querySelector("#Store");
 let MorshuLine = false;
 let storeExit = document.querySelector("#exitShop");
 let Win = false;
+const startButton = document.querySelector("#StartButton");
+let morshu = document.querySelector("#left");
+let shop = document.querySelector("#shop");
+let audio = document.querySelector('#audio');
 
+/*Shop select img */
+let bgimgs = document.querySelectorAll('.img');
+bgimgs.forEach(x =>{
+    x.addEventListener('click', Check);
+})
 
-/*game load */
-/*Az X és Y fellettek cserélve véletlen */
-for (let x = 0; x < 6; x++) {
-    for (let y = 0; y < 7; y++) {
-        table.innerHTML += `<div class="block" id="${x}-${y}"></div>`;
+function Check() {
+    let BankCard = document.querySelector('#creditCard').value;
+    if (BankCard.length != 0) {
+        InstallBG(this.id);
+    }
+    else{
+        morshu.style.backgroundImage = 'url(/Pics/ComeBackLater.gif)';
+        audio.innerHTML = '<audio src="/line2.mp3" controls="controls" style="display: none;" autoplay></audio>';
     }
 }
 
-console.log(document.getElementById("1"));
+function InstallBG(name) {
+    document.body.style.backgroundImage = `url('/Pics/${name}.png')`;
+}
+/*game load */
+/*Az X és Y fellettek cserélve véletlen */
+function Load() {
+    for (let x = 0; x < 6; x++) {
+        for (let y = 0; y < 7; y++) {
+            table.innerHTML += `<div class="block" id="${x}-${y}"></div>`;
+        }
+    }
 
-let cells = document.querySelectorAll('.block');
+    let cells = document.querySelectorAll('.block');
 
-cells.forEach(x => {
-    x.style.backgroundColor = 'lightgray';
-    x.addEventListener("click", Test);
-});
+    cells.forEach(x => {
+        x.style.backgroundColor = 'lightgray';
+        x.addEventListener("click", Test);
+    });
+}
+
 
 /*actual game */
 
@@ -40,9 +63,13 @@ function Test() {
                 CheckDiagnoally1(Id1, Id2, 'lightblue');
                 CheckDiagnoally2(Id1, Id2, 'lightblue');
                 Side = false;
-                round = false;
                 /*Win condition */
-                /*tie function */
+                if (Win == true) {
+                    Reset('lightblue');
+                }
+                else if (TieCheck()) {
+                    Reset('No one');
+                }
             }
             else{
                 this.style.backgroundColor = 'lightcoral';
@@ -51,13 +78,18 @@ function Test() {
                 CheckDiagnoally1(Id1, Id2, 'lightcoral');
                 CheckDiagnoally2(Id1, Id2, 'lightcoral');
                 Side = true;
-                round = false;
+                if (Win == true) {
+                    Reset('lightcoral')
+                }
+                else if (TieCheck()) {
+                    Reset('No one');
+                }
             }
+            
         }
         else{
             console.log("Foglalt bitch");
         }
-    round = true;
 }
 
 
@@ -97,8 +129,11 @@ let winCondition = true;
             winCondition = false;
         }
     }
+
+    if (winCondition == true) {
+        Win = true;
+    }
     console.log(`Horizontal check: ${winCondition}`);
-    return winCondition;
 }
 
 
@@ -138,8 +173,10 @@ function CheckVertically(Y, X, color) {
             winCondition = false;
         }
     }
+    if (winCondition == true) {
+        Win = true;
+    }
     console.log(`Vertical check: ${winCondition}`);
-    return winCondition;
 }
 
 
@@ -178,8 +215,10 @@ function CheckDiagnoally1(Y, X, color)
             winCondition = false;
         }
 
+        if (winCondition == true) {
+            Win = true;
+        }
     console.log(`Diagnoal1 check: ${winCondition}`);
-    return winCondition;
 }
 
 /*Fentről lefelé átló való check */
@@ -217,8 +256,10 @@ function CheckDiagnoally2(Y, X, color)
             winCondition = false;
         }
 
+        if (winCondition == true) {
+            Win = true;
+        }
     console.log(`Diagnoal2 check: ${winCondition}`);
-    return winCondition;
 }
 
 function TieCheck() {
@@ -233,6 +274,8 @@ function TieCheck() {
             }
         }
     }
+    console.log('tie:');
+    console.log(tie);
     return tie;
 }
 
@@ -241,11 +284,11 @@ store.addEventListener('click', Morshu);
 storeExit.addEventListener('click', Exit);
 
 function Morshu() {
-    document.querySelector("#shop").style.visibility = 'visible';
-    let morshu = document.querySelector("#left");
+    store.style.visibility = 'hidden';
+    shop.style.visibility = 'visible';
     if (MorshuLine == false) {
         morshu.style.backgroundImage = 'url(/Pics/Open_first.gif)';
-        document.querySelector('#audio').innerHTML = '<audio src="/line1.mp3" controls="controls" style="display: none;" autoplay></audio>';
+        audio.innerHTML = '<audio src="/line1.mp3" controls="controls" style="display: none;" autoplay></audio>';
         MorshuLine = true;
     }
     else{
@@ -254,6 +297,31 @@ function Morshu() {
 }
 
 function Exit() {
-    document.querySelector("#shop").style.visibility = 'hidden';
-    document.querySelector('#audio').innerHTML = '';
+    shop.style.visibility = 'hidden';
+    store.style.visibility = 'visible';
+    audio.innerHTML = '';
+}
+
+/*Start game */
+
+startButton.addEventListener('click', StartUp);
+
+function StartUp() {
+    Load();
+    table.style.visibility = 'visible';
+    startButton.style.visibility = 'hidden';
+    store.style.visibility = 'hidden';
+    document.querySelector('#Winner').style.visibility = 'hidden';
+}
+
+/*Reset Game */
+function Reset(WinnerName) {
+    Win = false;
+    Side = true;
+    table.innerHTML = '';
+    document.querySelector('#Winner').innerHTML = `${WinnerName} won!`;
+    document.querySelector('#Winner').style.visibility = 'visible';
+    table.style.visibility = 'hidden';
+    startButton.style.visibility = 'visible';
+    store.style.visibility = 'visible';
 }
